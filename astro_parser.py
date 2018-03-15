222880e37e9b566184251daa0dc030e10fe1c909
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2018-03-15 13:40:22 -0400

    Fixed authorship parsing
    Some authors names were formatted differently. The parser
    will now reformat all the author names to the same format

--- astro_parser.py
+++ astro_parser.py
@@ -16,6 +16,7 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 def main():
@@ -39,7 +40,7 @@ def main():
         if row_index > 0:
             # This condition is unnecessary; if you are testing
             # then drop the maximum row
-            if row_index < 1000:
+            if row_index < 400:
                 columns = row.find_all(["th", "td"])
                 clean_row = list()
                 # Cleaning and preprocessing
@@ -61,6 +62,25 @@ def main():
                         # Second column are the molecules
                         elif col_index == 1:
                             row_data.append(col[0])
+                        elif col_index == 2:
+                            # Loop over the references, and use regex to extract
+                            # the author names
+                            authors = list()
+                            for text in col:
+                                # Regex will pick up capital letter followed by period,
+                                # space, a capital letter and at least three lower case
+                                authors.extend(
+                                    re.findall(r"[A-Z]\.\s[A-Za-z]\w{3,}", text)
+                                )
+                                # Find all authors not formatted the same way
+                                authors.extend(
+                                    re.findall(r"[A-Z]\.[A-Za-z]\w{3,}", text)
+                                )
+                            # Remove whitespace
+                            authors = [author.replace(" ", "") for author in authors]
+                            # Make sure all authors are formatted the same way
+                            authors = [" ".join(author.split(".")) for author in authors]
+                            row_data.append(authors)
                         # Fourth column contains the source and technique
                         elif col_index == 3:
                             techs = list()
@@ -76,7 +96,7 @@ def main():
                     data.append(row_data)
 
     # Set up pandas dataframe
-    df = pd.DataFrame(data=data, columns=["Year", "Molecule", "Source", "Detection Method"])
+    df = pd.DataFrame(data=data, columns=["Year", "Molecule", "Authors", "Source", "Detection Method"])
 
     # Remove trans-HCOOCH3 until David figures out what he wants to do
     df = df[df["Molecule"] != "trans HCOOCH3"]
@@ -84,6 +104,38 @@ def main():
     # Dump to file
     df.to_csv("astrochymist.csv", index=False)
     return df
+
+def time_analysis(df):
+    # Perform analysis of detections of time, as well as
+    # the number of discoveries per author over time.
+    author_df = pd.DataFrame(df["Authors"].values.tolist(), index=df["Year"])
+    unique_authors = list()
+    # Create a list of unique authors
+    for column in author_df:
+        for author in author_df[column].unique():
+            if author not in unique_authors:
+                unique_authors.append(author)
+    # Dictionary for holding stuff
+    authorship = list()
+    tally = list()
+    running_tally = {author: 0 for author in unique_authors}
+    for index, row in df.iterrows():
+        row_data = {author: False for author in unique_authors}
+        for author in row["Authors"]:
+            row_data[author] = True
+        authorship.append(row_data)
+    authorship_df = pd.DataFrame(authorship, index=df["Year"])
+    # Collapse the dataframe along years
+    authorship_df = authorship_df.groupby(authorship_df.index).sum()
+    # Cumulative sum of authors over the years
+    tally_df = authorship_df.cumsum(axis=0)
+    tally_df.index = authorship_df.index
+    # Get the ranking of detections by authors
+    rank_df = authorship_df.sum().sort_values(ascending=False) 
+    authorship_df.to_csv("author_timeseries.csv")
+    tally_df.to_csv("author_cumsum.csv")
+    rank_df.to_csv("author_ranking.csv")
+    return authorship_df, tally_df, rank_df
     
 if __name__ == "__main__":
     df = main()
